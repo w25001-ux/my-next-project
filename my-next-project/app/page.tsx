901cb4760ec60aa34f6a78121e1f23d0125af05c
Aung Thi Ha <w25001@osfl.ac.jp>
2026-06-03 16:29:16 +0900

    サイト全体にrevalidateを設定する

--- my-next-project/app/page.tsx
+++ my-next-project/app/page.tsx
@@ -75,6 +75,8 @@ import ButtonLink from "./_components/BottonLink";
 //       }
 //     ],
 //         };
+
+export const revalidate = 60;
 export default async function Home(){
   // const sliceData = data.contents.slice(0,2);
 
